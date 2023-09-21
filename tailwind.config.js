7c9107202b11fc77b5f94e61ac2a4a4aae6406d6
Restyled.io <commits@restyled.io>
2023-09-21 06:19:09 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content :
-          [
-            './pages/**/*.{js,ts,jsx,tsx,mdx}',
-            './components/**/*.{js,ts,jsx,tsx,mdx}',
-            './app/**/*.{js,ts,jsx,tsx,mdx}',
-          ],
-  theme : {
-    screens : {
-      'sm' : '640px',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: "640px",
       // 'md': '768px', tablet
       // 'lg': '1024px', laptop
-      'mobile-sm' : '320px',
-      'mobile-md' : '375px',
-      'mobile-lg' : '425px',
-      'tablet' : '768px',
-      'laptop' : '1024px',
-      'xl' : '1280px',
-      'laptop-lg' : '1440px',
-      '2xl' : '1536px',
-      'desktop' : '2560px',
+      "mobile-sm": "320px",
+      "mobile-md": "375px",
+      "mobile-lg": "425px",
+      tablet: "768px",
+      laptop: "1024px",
+      xl: "1280px",
+      "laptop-lg": "1440px",
+      "2xl": "1536px",
+      desktop: "2560px",
     },
   },
-  plugins : []
-}
+  plugins: [],
+};
