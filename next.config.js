85ff39e43afbff5e54d26ce36a71e543b6152d9d
Restyled.io <commits@restyled.io>
2023-09-21 06:19:07 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-        pathname: '/uploads/**',
-      }
-    ]
+  images : {
+    unoptimized : true,
+    remotePatterns : [ {
+      protocol : 'http',
+      hostname : '127.0.0.1',
+      port : '1337',
+      pathname : '/uploads/**',
+    } ]
   }
 }
 
-module.exports = nextConfig
+                   module.exports = nextConfig
